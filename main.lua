13785e33f7d325a5d7efe8abc2bdf61d14c42c22
3exotic <x7f9l2p8qw3r6t1y@gmail.com>
2026-05-11 15:47:46 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local aaa = true
 
 if aaa then
     loadstring(game:HttpGet("https://alpha.vonalia.com/api/v1/scripts/1778532190763"))()
+    print("loading")
 else
     warn("aaa is false, not loading")
 end
